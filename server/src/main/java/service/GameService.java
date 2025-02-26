5e8e0d5369ae5aa378002591eae6bb03d95b0e89
joeldemars <joeltdemars@gmail.com>
2025-02-26 15:06:45 -0700

    Implement GameService.createGame

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,16 +1,33 @@
 package service;
 
+import chess.ChessGame;
+import dataaccess.DataAccessException;
 import dataaccess.MemoryGameDAO;
+import model.GameData;
 
 public class GameService {
     private MemoryGameDAO games = new MemoryGameDAO();
+    private int gameID = 0;
 
     public ListGamesResult listGames() throws InternalServerErrorException {
         throw new InternalServerErrorException("Not implemented");
     }
 
     public int createGame(String gameName) throws BadRequestException, InternalServerErrorException {
-        throw new InternalServerErrorException("Not implemented");
+        try {
+            if (games.listGames().stream().anyMatch((game) -> game.gameName().equals(gameName))) {
+                throw new BadRequestException("Error: bad request");
+            } else {
+                try {
+                    games.createGame(new GameData(gameID, "", "", gameName, new ChessGame()));
+                    return gameID++;
+                } catch (DataAccessException e) {
+                    throw new InternalServerErrorException("Error: failed to create game");
+                }
+            }
+        } catch (DataAccessException e) {
+            throw new InternalServerErrorException("Error: failed to create game");
+        }
     }
 
     public void joinGame(JoinGameRequest request)
