9f6f2cb80966e58fc772eed65f887bf10fa47811
joeldemars <joeltdemars@gmail.com>
2025-02-28 17:04:50 -0700

    Implement ResetService tests

--- server/src/test/java/service/ResetServiceTests.java
+++ server/src/test/java/service/ResetServiceTests.java
@@ -1,6 +1,9 @@
 package service;
 
+import dataaccess.*;
+import model.UserData;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,15 +12,39 @@ public class ResetServiceTests {
     private GameService gameService;
     private ResetService resetService;
 
+    @BeforeEach
+    public void resetService() {
+        UserDAO users = new MemoryUserDAO();
+        AuthDAO auths = new MemoryAuthDAO();
+        GameDAO games = new MemoryGameDAO();
+
+        userService = new UserService(users, auths);
+        gameService = new GameService(games, auths);
+        resetService = new ResetService(users, auths, games);
+    }
+
     @Test
     @DisplayName("Successfully clear databases")
     public void clearDatabases() {
-        Assertions.fail("Not implemented");
+        RegisterRequest registerRequest =
+                new RegisterRequest("user", "secret", "email@mail.com");
+        String authToken = userService.register(registerRequest).authToken();
+        gameService.createGame("Game", authToken);
+        resetService.clearDatabase();
+        Assertions.assertDoesNotThrow(() -> {
+            userService.register(registerRequest);
+        }, "Failed to register duplicate user after clearing databases");
+        authToken = userService.login(new LoginRequest("user", "secret")).authToken();
+        Assertions.assertEquals(0, gameService.listGames(authToken).games().length,
+                "Games found after clearing databases");
     }
 
     @Test
     @DisplayName("Successfully clear empty databases")
     public void clearEmptyDatabases() {
-        Assertions.fail("Not implemented");
+        Assertions.assertDoesNotThrow(() -> {
+            resetService.clearDatabase();
+            resetService.clearDatabase();
+        }, "Failed to clear empty databases");
     }
 }
