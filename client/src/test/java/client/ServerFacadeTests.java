fe2e21de96fccbaba945464823e258fca575c81d
joeldemars <joeltdemars@gmail.com>
2025-03-24 11:00:15 -0600

    Stub out ServerFacadeTests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,5 +1,6 @@
 package client;
 
+import jdk.jfr.Description;
 import org.junit.jupiter.api.*;
 import server.Server;
 
@@ -20,10 +21,87 @@ public class ServerFacadeTests {
         server.stop();
     }
 
-
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    @Description("Successfully clear databases")
+    public void clearDatabases() {
+        Assertions.fail("Not implemented");
     }
 
+    @Test
+    @Description("Successfully clear empty databases")
+    public void clearEmptyDatabases() {
+        Assertions.fail("Not implemented");
+    }
+
+    @Test
+    @Description("Successfully register new user")
+    public void registerUser() {
+        Assertions.fail("Not implemented");
+    }
+
+    @Test
+    @Description("Fail to register duplicate user")
+    public void registerDuplicateUser() {
+        Assertions.fail("Not implemented");
+    }
+
+    @Test
+    @Description("Successfully log in as existing user")
+    public void logIn() {
+        Assertions.fail("Not implemented");
+    }
+
+    @Test
+    @Description("Fail to log in with incorrect credentials")
+    public void logInWithIncorrectCredentials() {
+        Assertions.fail("Not implemented");
+    }
+
+    @Test
+    @Description("Successfully log out")
+    public void logOut() {
+        Assertions.fail("Not implemented");
+    }
+
+    @Test
+    @Description("Fail to log out before logging in")
+    public void logOutWithoutLoggingIn() {
+        Assertions.fail("Not implemented");
+    }
+
+    @Test
+    @Description("Successfully list one game")
+    public void listOneGame() {
+        Assertions.fail("Not implemented");
+    }
+
+    @Test
+    @Description("Fail to list games without logging in")
+    public void listGamesWithoutLoggingIn() {
+        Assertions.fail("Not implemented");
+    }
+
+    @Test
+    @Description("Successfully create new game")
+    public void createNewGame() {
+        Assertions.fail("Not implemented");
+    }
+
+    @Test
+    @Description("Fail to create duplicate game")
+    public void createDuplicateGame() {
+        Assertions.fail("Not implemented");
+    }
+
+    @Test
+    @Description("Successfully join game")
+    public void joinGame() {
+        Assertions.fail("Not implemented");
+    }
+
+    @Test
+    @Description("Fail to join nonexistent game")
+    public void joinNonexistentGame() {
+        Assertions.fail("Not implemented");
+    }
 }
